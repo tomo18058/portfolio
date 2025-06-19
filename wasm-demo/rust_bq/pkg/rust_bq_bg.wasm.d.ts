4cda787549844690f7b7fff436aa1721fbc578af
原知輝 <tomontomon1203@icloud.com>
2025-06-19 12:01:45 +0900

    deploy: build and push to GitHub Pages

---
+++ wasm-demo/rust_bq/pkg/rust_bq_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const run_sort_once: (a: number) => [number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
