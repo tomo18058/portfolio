8b24618892a760bc64bfa1bab82b8da413189f5c
原知輝 <tomontomon1203@icloud.com>
2025-09-18 16:57:49 +0900

    deploy: update portfolio

---
+++ src/pages/self-inttoduction.tsx
@@ -0,0 +1,9 @@
+export const Selfintroduction = () => {
+    return (
+        <div className="text-center">
+            <h1 className="mb-4 text-4xl font-bold">自己紹介</h1>
+            <p>こんにちは！TOMOKIです。</p>
+            <p>ReactとRustが大好きな開発者です。</p>
+        </div>
+    );
+};
